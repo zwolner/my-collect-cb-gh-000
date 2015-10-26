498363592cc418e6ac079c90f3a2be3ea0c589dd
joshuabamboo <joshuaowens011@gmail.com>
2015-10-26 15:40:02 -0400

    update tests to remove confusing proc: issue #6

--- spec/my_collect_spec.rb
+++ spec/my_collect_spec.rb
@@ -3,12 +3,11 @@ describe "my_collect" do
 
   it "can handle an empty collection" do
     empty_array = []
-    saved_block = Proc.new {
+    counter = 0
       my_collect(empty_array) do |x|
-        raise "This block should not run!"
+        counter += 1
       end
-    }
-    expect(saved_block).to_not raise_error
+    expect(counter).to eq(0)
   end
 
   it "yields the correct element" do
