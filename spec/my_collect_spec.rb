0ae9691ef44ecd17850d4f4e41c7c959a680997b
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2016-06-27 11:31:04 -0400

    fix issue 11 readme uses student collection as example, test suite uses languages. method should be abstract--yield members of any given collection to any given block. clarified this in readme and added tests for another case--both langauges behavior and students

--- spec/my_collect_spec.rb
+++ spec/my_collect_spec.rb
@@ -1,5 +1,6 @@
 describe "my_collect" do
   let(:languages) { ['ruby', 'javascript', 'python', 'objective-c'] }
+  let(:students) { ['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller'] }
 
   it "can handle an empty collection" do
     empty_array = []
@@ -10,13 +11,13 @@ describe "my_collect" do
     expect(counter).to eq(0)
   end
 
-  it "yields the correct element" do
+  it "yields the correct element from a given collection, in this case languages" do
     my_collect(languages) do |language|
       expect(language).to_not eq(nil)
     end
   end
 
-  it "returns a new collection" do
+  it "returns a new collection of appropriately modified elements, in this case capitalized languages" do
     expect(my_collect(languages) do |language|
       language.upcase
     end).to eq(["RUBY", "JAVASCRIPT", "PYTHON", "OBJECTIVE-C"])
@@ -27,5 +28,24 @@ describe "my_collect" do
       language.upcase
     end
     expect(languages).to eq(['ruby', 'javascript', 'python', 'objective-c'])
+  end
+
+  it "yields the correct element from the given collection, in this case students" do
+    my_collect(students) do |student|
+      expect(student).to_not eq(nil)
+    end
+  end
+
+  it "returns a new collection of appropriately modified elements, in this case student first names" do
+    expect(my_collect(students) do |language|
+      student.split(" ").first
+    end).to eq(["Tim", "Tom", "Sophie", "Antoin"])
+  end
+
+  it 'does not modify the original collection' do
+    my_collect(students) do |language|
+      language.upcase
+    end
+    expect(students).to eq(['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller'])
   end
 end
